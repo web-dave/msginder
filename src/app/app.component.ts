1465aa583938247b8990931cdcb998adee548929
web-dave <dave@webdave.de>
2021-08-30 08:41:20 +0200

    first try

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { data, fields, filter, names } from './messengermatrix';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'msginder';
+  step = 0;
+  messengers = data;
+  fields: { [key: string]: string[] } = fields;
+  keys = Object.keys(fields);
+  filter: { [key: string]: string } = filter;
+  texte = names;
+
+  setFilter(key: string, val: string) {
+    this.filter[key] = val;
+    this.step++;
+    this.doesItMatch(this.messengers[0]);
+  }
+  allowDrop(ev: Event) {
+    ev.preventDefault();
+  }
+  doesItMatch(value: { [key: string]: string }): boolean {
+    let match = true;
+    for (let key of this.keys) {
+      if (this.filter[key] !== '') {
+        console.log(
+          key,
+          '|',
+          this.filter[key],
+          '|',
+          value[key],
+          '|',
+          this.filter[key].includes(value[key])
+        );
+        if (!this.filter[key].includes(value[key])) {
+          return false;
+        }
+      }
+    }
+    return match;
+  }
 }
